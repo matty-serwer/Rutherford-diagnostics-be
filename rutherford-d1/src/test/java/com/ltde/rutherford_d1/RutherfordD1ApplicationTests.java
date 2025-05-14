64b711eefc5b2388b29686a5151df60217f2b68e
Matty Serwer <Matthew.Serwer@gmail.com>
2025-05-14 17:17:19 -0400

    Connects and tests connection to local posgresql server through hibernate. Creates a basic data loader for seeding data.

--- rutherford-d1/src/test/java/com/ltde/rutherford_d1/RutherfordD1ApplicationTests.java
+++ rutherford-d1/src/test/java/com/ltde/rutherford_d1/RutherfordD1ApplicationTests.java
@@ -2,9 +2,13 @@ package com.ltde.rutherford_d1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootTest
 class RutherfordD1ApplicationTests {
+
+	private static final Logger log = LoggerFactory.getLogger(DataLoader.class);
 
 	@Test
 	void contextLoads() {
